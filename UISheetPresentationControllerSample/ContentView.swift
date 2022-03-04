c6f6a38e91676202bfd426e3df8f7460cd10eaa6
tsurumoto <tsurumotokentarou@gmail.com>
2022-03-04 23:16:07 +0900

    Initial Commit

---
+++ UISheetPresentationControllerSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UISheetPresentationControllerSample
+//
+//  Created by 鶴本賢太朗 on 2022/03/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UISheetPresentationControllerSample/UISheetPresentationControllerSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  UISheetPresentationControllerSampleApp.swift
+//  UISheetPresentationControllerSample
+//
+//  Created by 鶴本賢太朗 on 2022/03/04.
+//
+
+import SwiftUI
+
+@main
+struct UISheetPresentationControllerSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
